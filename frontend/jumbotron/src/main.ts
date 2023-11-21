4a48cd42b791e4aea5994bdef79aa35ec8680b3d
Matthew Vandenberg <mv5903@gmail.com>
2023-11-21 13:19:09 -0500

    test

--- frontend/jumbotron/src/main.ts
+++ frontend/jumbotron/src/main.ts
@@ -1,8 +1,13 @@
 import './app.css'
 import App from './App.svelte'
 
+
 const app = new App({
   target: document.getElementById('app'),
 })
+
+if (typeof global === 'undefined') {
+  var global = window;
+}
 
 export default app
