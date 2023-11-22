6f15a851b0af72be789f78ac1cfdfc23c963409b
Matthew Vandenberg <mv5903@gmail.com>
2023-11-21 19:32:14 -0500

    disconnect from scoket on disconnect

--- frontend/jumbotron/src/classes/Jumbotron.ts
+++ frontend/jumbotron/src/classes/Jumbotron.ts
@@ -78,6 +78,7 @@ export class Jumbotron {
             port: 5000,
             isInitialized: false,
         } as Jumbotron);
+        this.socket.disconnect();
     }
 
     async setupPixels() {
