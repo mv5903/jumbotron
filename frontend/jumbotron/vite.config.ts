93ddbbbc9415b966e860a665975ac3e0c06583d7
Matthew Vandenberg <mv5903@gmail.com>
2024-10-02 16:51:37 -0400

    try some browserify resolvers

--- frontend/jumbotron/vite.config.ts
+++ frontend/jumbotron/vite.config.ts
@@ -4,7 +4,21 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  server: {
-    host: true
-  }
+  define: {
+    "global": {},
+  },
+  resolve: {
+      alias: {
+          process: "process/browser",
+          buffer: "buffer",
+          crypto: "crypto-browserify",
+          stream: "stream-browserify",
+          assert: "assert",
+          http: "stream-http",
+          https: "https-browserify",
+          os: "os-browserify",
+          url: "url",
+          util: "util",
+      },
+  },
 })
